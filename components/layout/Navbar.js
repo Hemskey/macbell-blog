0385d8d45ce7eb62b13192f78c11a8f46f947fe4
Dan <dan.tran301@gmail.com>
2021-07-01 17:08:47 -0700

    Hamburger menu
    Repair disappearing hamburger menu on medium and smaller screens. Also
    close menu on click of navigation links.

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -12,24 +12,29 @@ function Navbar() {
     setOpen((prevOpen) => !prevOpen);
   };
 
+  const closeMenuOnLinkClick = () => {
+    setOpen(false);
+  };
+
   return (
     <header className="flex flex-row justify-between mt-2 items-center w-full">
       <button className="focus:outline-none focus:border-transparent">
-        <FontAwesomeIcon
-          className="ml-6 md:hidden block text-md"
-          icon={faBars}
+        <img
           onClick={handleMenuOpening}
+          className="ml-6 md:hidden block text-md"
+          width="20"
+          src="https://img.icons8.com/material-outlined/24/000000/menu--v1.png"
         />
       </button>
       <ul
         className={`${
           open ? "block" : "hidden"
-        } absolute pt-0 top-24 left-0 flex-col w-full p-6 bg-white shadow-md rounded-lg z-20 md:relative md:top-0 md:z-0 md:flex md:flex-row md:space-x-3 md:rounded-none md:shadow-none md:bg-transparent md:p-6 md:justify-end text-xs tracking-widest md:mr-28 lg:mr-64 md:ml-1 lg:ml-1 xl:ml-0 xl:mr-72`}
+        } absolute pt-0 top-24 left-0 flex-col w-full p-6 -space-y-0.5 md:space-y-0 bg-white shadow-md rounded-lg z-20 md:relative md:top-0 md:z-0 md:flex md:flex-row md:space-x-3 md:rounded-none md:shadow-none md:bg-transparent md:p-6 md:justify-end text-xs tracking-widest md:mr-28 lg:mr-64 md:ml-1 lg:ml-1 xl:ml-0 xl:mr-72`}
       >
-        <li>
+        <li onClick={closeMenuOnLinkClick}>
           <Link href="/">HOME</Link>
         </li>
-        <li>
+        <li onClick={closeMenuOnLinkClick}>
           <Link href="/posts">BLOG</Link>
         </li>
         <li className="text-xs tracking-tighter whitespace-nowrap hidden md:block">
@@ -50,12 +55,12 @@ function Navbar() {
       <ul
         className={`${
           open ? "block" : "hidden"
-        } absolute top-32 left-0 z-20 flex-col w-full bg-white shadow-md rounded-lg p-6 md:relative md:top-0 md:flex md:flex-row md:space-x-3 md:rounded-none md:shadow-none md:bg-transparent md:ml-28 lg:ml-56 xl:ml-72 md:p-6 pt-0 text-xs tracking-widest md:justify-start`}
+        } absolute top-32 left-0 z-20 flex-col w-full -space-y-0.5 md:space-y-0 bg-white shadow-md rounded-lg p-6 md:relative md:top-0 md:flex md:flex-row md:space-x-3 md:rounded-none md:shadow-none md:bg-transparent md:ml-28 lg:ml-56 xl:ml-72 md:p-6 pt-0 text-xs tracking-widest md:justify-start`}
       >
-        <li>
+        <li onClick={closeMenuOnLinkClick}>
           <Link href="/contact">CONTACT</Link>
         </li>
-        <li className="whitespace-nowrap">
+        <li onClick={closeMenuOnLinkClick} className="whitespace-nowrap">
           <Link href="/socialmedia">SOCIAL MEDIA</Link>
         </li>
       </ul>
