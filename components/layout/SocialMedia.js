b54ff88b2021ddf5dbe65b0eb2f72ebdeeca73aa
Dan Tran <65366053+dtran5@users.noreply.github.com>
2021-08-03 15:31:15 -0700

    Update SocialMedia.js

--- components/layout/SocialMedia.js
+++ components/layout/SocialMedia.js
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-function SocialMedia({ image, name }) {
+function SocialMedia({ image, name, position }) {
   return (
     <div className="flex flex-col items-center">
       <div className="relative h-80 w-80 flex flex-col mb-3 mt-5">
@@ -13,6 +13,7 @@ function SocialMedia({ image, name }) {
               layout="fill"
               className="rounded-full"
               objectFit="cover"
+              objectPosition={position}
             />
           </a>
         </Link>
