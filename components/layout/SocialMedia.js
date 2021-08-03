1f5b29fb36a31946a42495d837f3cb19629bac58
Dan <dan.tran301@gmail.com>
2021-08-03 16:26:25 -0700

    Add social media images to the social media page

--- components/layout/SocialMedia.js
+++ components/layout/SocialMedia.js
@@ -2,17 +2,18 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-function SocialMedia({ image, name }) {
+function SocialMedia({ image, name, position, link }) {
   return (
     <div className="flex flex-col items-center">
       <div className="relative h-80 w-80 flex flex-col mb-3 mt-5">
-        <Link href="https://www.instagram.com/hellomariko/">
+        <Link href={link}>
           <a>
             <Image
               src={image}
               layout="fill"
               className="rounded-full"
               objectFit="cover"
+              objectPosition={position}
             />
           </a>
         </Link>
