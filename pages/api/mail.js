182ac2ee77660cf539b2614a18c4c64f913d2786
Dan Tran <65366053+dtran5@users.noreply.github.com>
2021-08-04 10:40:02 -0700

    Update mail.js

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,25 +1,25 @@
-const mail = require("@sendgrid/mail");
+// const mail = require("@sendgrid/mail");
 
-mail.setApiKey(`${process.env.SENDGRID_API_KEY}`);
+// mail.setApiKey(`${process.env.SENDGRID_API_KEY}`);
 
-export default (req, res) => {
-  const body = JSON.parse(req.body);
-  const message = `
-  Name: ${body.firstName} ${body.lastName}\r\n
-  Email: ${body.email}\r\n
-  Message: ${body.message}
-  `;
+// export default (req, res) => {
+//   const body = JSON.parse(req.body);
+//   const message = `
+//   Name: ${body.firstName} ${body.lastName}\r\n
+//   Email: ${body.email}\r\n
+//   Message: ${body.message}
+//   `;
 
-  const data = {
-    to: "themacbellclan@gmail.com",
-    from: "Blog@macbellclan.com",
-    subject: "Contact Form Message",
-    text: message,
-    html: message.replace(/\r\n/g, "<br>"),
-  };
+//   const data = {
+//     to: "themacbellclan@gmail.com",
+//     from: "Blog@macbellclan.com",
+//     subject: "Contact Form Message",
+//     text: message,
+//     html: message.replace(/\r\n/g, "<br>"),
+//   };
 
-  mail.send(data);
-  console.log(body);
+//   mail.send(data);
+//   console.log(body);
 
-  res.status(200).json({ status: "okay" });
-};
+//   res.status(200).json({ status: "okay" });
+// };
