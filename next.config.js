dc43a31990a460adb44371ee9659a392713a4573
Dan Tran <65366053+dtran5@users.noreply.github.com>
2021-08-04 21:19:58 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    mongodb_username: "macbelluser",
-    mongodb_password: "macbell",
-    mongodb_clustername: "macbell-cluster",
-    mongodb_database: "macbell-blog",
-    SERVICE_ID: "service_1znn0ft",
-    TEMPLATE_ID: "template_tdvwrpp",
-    USER_ID: "user_8BS2SDRPWk0wT8Rtohmk2",
-  },
+  
 };
