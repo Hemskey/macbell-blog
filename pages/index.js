6dfdfeb5af6b77c904fa074e8d2c825a3b6dcf6e
campkim <96302910+campkim@users.noreply.github.com>
2025-07-29 13:06:43 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+timport Head from "next/head";
 
 import styles from "../styles/Home.module.css";
 const homePageContent = [
@@ -7,7 +7,7 @@ const homePageContent = [
     paragraph1:
       "Our little corner of the internet includes stories and adventures from our little life. We thought it would be a fun way to write down our thoughts, be a little more intentional with our actions, and look back on our memories. We are a happy bunch and are happy you’re along for the ride.",
     paragraph2:
-      "We live in beautiful , WA in our new house! We are both full time corporate American (And Canadian) workers but when we have time off we use that for travel, learning, and fun hobbies. As you see the theme of our blog change over time, it reflects our learning and interests as they shift. ",
+      "We live in beautiful Seattle, WA in our new house! We are both full time corporate American (And Canadian) workers but when we have time off we use that for travel, learning, and fun hobbies. As you see the theme of our blog change over time, it reflects our learning and interests as they shift. ",
   },
   {
     image: "home-1.jpg",
